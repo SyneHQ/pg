a0c6ef193a7c204587dc398509178f4077b04fcd
theboringhumane <me@iamharsh.dev>
2025-02-23 12:43:48 +0530

    Remove pg_qualstats and pg_stat_kcache extensions from PostgreSQL configuration
    - Simplify PostgreSQL monitoring extensions to only pg_stat_statements
    - Remove additional query and I/O statistics tracking extensions
    - Update init.sql and postgresql.conf to reflect reduced extension set

--- init.sql
+++ init.sql
@@ -1,6 +1,2 @@
 CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
 COMMENT ON EXTENSION pg_stat_statements IS 'track execution time, query count, and memory usage of all SQL statements';
-CREATE EXTENSION IF NOT EXISTS pg_qualstats;
-COMMENT ON EXTENSION pg_qualstats IS 'track query execution details and statistics';
-CREATE EXTENSION IF NOT EXISTS pg_stat_kcache;
-COMMENT ON EXTENSION pg_stat_kcache IS 'track I/O statistics for queries';
